788cd2ba26ad2f8e474a556e5384182f0e9ae09c
jairo5200 <jairo5200@gmail.com>
2025-03-03 18:42:17 -0500

    se listan los products

--- resources/js/Pages/products/Index.tsx
+++ resources/js/Pages/products/Index.tsx
@@ -1,22 +1,36 @@
-// resources/js/Pages/products/Index.js
 import React from 'react';
+import AppLayout from '@/Layouts/AppLayout';
 
-const Index = ({ products } :any) => {
+export default function Products({products}) {
   return (
-    <div>
-      <h1>Lista de Productos</h1>
-      <ul>
-        {products.map((product: any) => (
+    <AppLayout
+      title="Productos"
+      renderHeader={() => (
+        <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+          Productos
+        </h2>
+      )}
+    >
+      <div className="py-12">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6">
+            <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-200">
+              Lista de productos
+            </h1>
+            <p className="text-gray-600 dark:text-gray-400">
+            {products.map((product: any) => (
           <div>
-            <li key={product.id}>{product.name}</li> 
-            <li key={product.id}>{product.description}</li> 
+            <li key={product.id}>{product.name}</li>
+            <li key={product.id}>{product.description}</li>
             <img src={product.image} alt="golulu" />
           </div>
-          
-        ))}
-      </ul>
-    </div>
-  );
-};
 
-export default Index;
+        ))}
+            </p>
+          </div>
+        </div>
+      </div>
+    </AppLayout>
+  );
+}
+
