af91160b1cb0c25aa2e7cb52e796364fbc093847
Francisco <jesusfrancisco7676@hotmail.com>
2025-03-11 16:17:22 -0500

    correcion index user, correccion index products

--- resources/js/Pages/products/Index.tsx
+++ resources/js/Pages/products/Index.tsx
@@ -38,7 +38,7 @@ export default function Products({ products }: any) {
         <div className='bg-white dark:bg-gray-800 shadow-xl sm:rounded-lg p-6'>
           <div className="flex justify-between items-center mb-4">
             <h1 className="text-2xl font-bold text-gray-800 dark:text-white">Lista de Productos</h1>
-            <div className='flex space-x-4 items-center'>
+            <div className='flex items-center'>
               <div>
                 <BarraBusqueda setSearchTerm={setSearchTerm} searchTerm={searchTerm} />
               </div>
