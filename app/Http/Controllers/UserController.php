c268fa24fdd43e4d0b29433384de2419bbf2bc17
Francisco <jesusfrancisco7676@hotmail.com>
2025-03-19 16:45:35 -0500

    optimizacion frontedt products

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Log;
 
 class UserController extends Controller
 {
--- resources/js/Pages/products/Index.tsx
+++ resources/js/Pages/products/Index.tsx
@@ -45,17 +45,14 @@ export default function Products({ products }: any) {
   };
 
   const confirmDeleteProduct = () => {
-    if (selectedProduct) {
       deleteProduct(route('products.destroy', selectedProduct.id), {
         onSuccess: () => {
           setShowDeleteModal(false);
-          console.log('Producto eliminado');
         },
         onError: (errors) => {
           console.error('Error al eliminar el producto', errors);
         },
       });
-    }
   };
 
   const handleAddProduct = () => setShowAddProductModal(true);
@@ -234,7 +231,7 @@ export default function Products({ products }: any) {
                                   <div className='flex items-center justify-center w-full'>
                                       <label className='flex flex-col border-4 border-dashed w-full h-32 hover:bg-gray-100 hover:border-purple-300 group'>
                                           <div className='flex flex-col items-center justify-center pt-7'>
-                                          <svg className="w-10 h-10 text-purple-400 group-hover:text-purple-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+                                          <svg className="w-10 h-10 text-purple-400 group-hover:text-purple-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
                                           <p className='text-sm text-gray-400 group-hover:text-purple-600 pt-1 tracking-wider'>Seleccione la imagen</p>
                                           </div>
                                       <input name="image" id="image" type='file' className="hidden" onChange={handleChange} />
